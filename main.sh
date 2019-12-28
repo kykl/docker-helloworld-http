b5af4a6d849df930c1d7b4648661b42c420e783c
Kenneth Lee <kykl@users.noreply.github.com>
2019-12-27 23:33:50 -0800

    use port 8088

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo '<html><head><title>HTTP Hello World</title><meta name="google-site-verification" content="DIbqkg6YyRUral399MOSijd7mrE-27M41jsnyjMXc58"/></head><body><h1>Hello from '$(hostname)'</h1></body></html' > /www/index.html
-python -m SimpleHTTPServer 80
+python -m SimpleHTTPServer 8088
