16e5b8ceaedde5e523678f614523a6c3a484c1a3
Kenneth Lee <kykl@users.noreply.github.com>
2019-12-27 23:11:54 -0800

    added meta tag for google domain verification

--- main.sh
+++ main.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-echo '<html><head><title>HTTP Hello World</title></head><body><h1>Hello from '$(hostname)'</h1></body></html' > /www/index.html
-
+echo '<html><head><title>HTTP Hello World</title><meta name="google-site-verification" content="DIbqkg6YyRUral399MOSijd7mrE-27M41jsnyjMXc58"/></head><body><h1>Hello from '$(hostname)'</h1></body></html' > /www/index.html
 python -m SimpleHTTPServer 80
